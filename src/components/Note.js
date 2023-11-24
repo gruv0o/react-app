e34c1c7f786fb4d7e170e6c69bd6e387ee0510b5
gruv0o <renaultremi51100@gmail.com>
2023-11-24 16:57:26 +0100

    Recup les infos des notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,44 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import "./Note.css";
 
 function Note() {
-  const { id } = useParams();
+const { id } = useParams();
+const [note, setNote] = useState(null);
+
+async function fetchNote(){
+  const response = await fetch('/notes/'+id);
+  const data = await response.json();
+  setNote(data);
+}
+
+useEffect(() =>{
+
+  fetchNote();
+
+},[id]);
+
+if(!note){
+  return "Chargement..."
+}
+
 
   return (
     <form className="Form">
-      <input className="Note-editable Note-title" type="text" />
-      <textarea className="Note-editable Note-content" />
+      <input className="Note-editable Note-title" 
+      type="text" 
+      value={note.title}
+      //onChange={}
+      />
+      <textarea 
+      className="Note-editable Note-content"
+      value={note.content}
+      //onChange={}
+      />
       <div className="Note-actions">
         <button className="Button">Enregistrer</button>
-      </div>
+      </div>  
     </form>
   );
 }
