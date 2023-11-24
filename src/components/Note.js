7cf25c913e4220742cb453d5b9aad46256ded12c
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-11-24 14:06:15 +0100

    Add Note form skeleton

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
 
+import "./Note.css";
+
 function Note() {
   const { id } = useParams();
 
-  return id;
+  return (
+    <form className="Form">
+      <input className="Note-editable Note-title" type="text" />
+      <textarea className="Note-editable Note-content" />
+      <div className="Note-actions">
+        <button className="Button">Enregistrer</button>
+      </div>
+    </form>
+  );
 }
 
 export default Note;
